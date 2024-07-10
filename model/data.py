6583add1445ad1139e1d9986b3d9d03f0554811d
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2024-07-10 19:14:14 +0200

    Data: Load more population data in

--- model/data.py
+++ model/data.py
@@ -1,6 +1,7 @@
 import pickle
 import networkx as nx
-from pandas import read_pickle
+import pandas as pd
+import geopandas as gpd
 
 
 class Data:
@@ -17,12 +18,31 @@ class Data:
         with open("../data/polygons.pkl", "rb") as f:
             self.city_polygon, self.area_polygon = pickle.load(f)
 
-        # Load the population data geodate
-        self.pc4_pop = read_pickle("../data/population_data_pc4.pkl")
-        self.mrdh_pop = read_pickle("../data/mrdh_pop.pickle")
+        self.city_polygon_series = gpd.GeoSeries(self.city_polygon, crs="epsg:4326")
+        self.city_polygon_series = self.city_polygon_series.to_crs(epsg=28992)
+
+        self.area_polygon_series = gpd.GeoSeries(self.area_polygon, crs="epsg:4326")
+        self.area_polygon_series = self.area_polygon_series.to_crs(epsg=28992)
+
+        # Load the population data geodataframes
+        # On PC4 level (postal code areas)
+        self.pc4_gdf = pd.read_pickle("../data/population_data_pc4_65coded.pkl")
+
+        # On 65-area level from V-MRDH
+        self.mrdh65_gdf = pd.read_pickle("../v_mrdh/areas_mrdh.pkl")
 
         # Load the road network
         self.road_network = nx.read_graphml("../network/graphs/merged_network.graphml")
 
+        # For both pc4 and mrdh add a column if the centroid is in the target_area
+        for target_area, polygon in zip(["in_city", "in_area"], [self.city_polygon_series, self.area_polygon_series]):
+            self.pc4_gdf[target_area] = self.pc4_gdf.centroid.within(polygon.geometry[0])
+            self.mrdh65_gdf[target_area] = self.mrdh65_gdf.centroid.within(polygon.geometry[0])
+
+        # Create a dictionary of pc4 and mrdh65 centroids: index mapped to Geodataframe centroid
+        self.pc4_centroids = {pc4: centroid for pc4, centroid in self.pc4_gdf.centroid.items()}
+        self.mrdh_centroids = {mrdh: centroid for mrdh, centroid in self.mrdh65_gdf.centroid.items()}
+        print(f"{len(self.mrdh_centroids)} mrhd_centroids: {self.mrdh_centroids}")
+
 # Initialize the data
-# data = Data()
+data = Data()
