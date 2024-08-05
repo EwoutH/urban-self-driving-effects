8832e4758f6bece694074dbf14ac619bf37ad6ed
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2024-08-05 10:45:21 +0200

    model: Use discrete event scheduling
    See https://github.com/projectmesa/mesa/blob/main/mesa/experimental/devs

--- model/agent.py
+++ model/agent.py
@@ -2,6 +2,8 @@ from mesa import Agent
 from data import Data
 from shapely.geometry import Point
 
+import numpy as np
+
 class Traveler(Agent):
     def __init__(self, unique_id, model, pc4, mrdh65):
         super().__init__(unique_id, model)
@@ -18,8 +20,19 @@ class Traveler(Agent):
         self.pc4 = pc4
         self.mrdh65 = mrdh65
 
-    def plan_journey(self):
-        pass
+        self.trip_times = []
+
+    def generate_trip_times(self):
+        # Generate 3 random trip times between the model start and end time.
+        self.trip_times = [self.model.start_time + (self.model.end_time - self.model.start_time) * np.random.random() for _ in range(3)]
+        self.trip_times.sort()
+
+        # Schedule events for the trip times (use self.model.simulator.schedule_event_absolute)
+        for trip_time in self.trip_times:
+            self.model.simulator.schedule_event_absolute(function=self.perform_journey, time=trip_time)
+
+    def perform_journey(self):
+        print(f"Agent {self.unique_id} is performing a journey! Model time = {self.model._time}, simulator time = {self.model.simulator.time}")
         # Get a destination
 
 
--- model/model.py
+++ model/model.py
@@ -4,15 +4,25 @@ from data import Data
 
 # package imports
 from mesa import Model
+from mesa.experimental.devs.simulator import DEVSimulator
 import numpy as np
+from datetime import datetime
 
 data = Data()
 
 
 class UrbanModel(Model):
 
-    def __init__(self, n_agents=5):
+    def __init__(self, n_agents=5, step_time=1/12, start_time=6, end_time=22, simulator=None):
         super().__init__()
+        # Set up simulator time
+        self.simulator = simulator
+        self.simulator.time = float(start_time)
+
+        # Set up time variables
+        self.step_time = step_time
+        self.start_time = start_time
+        self.end_time = end_time
         # Create a dictionary of locations pc4 locations and their populations from pop_gdf_nl_pc4 with in_city == True
         gdf = data.pop_gdf_nl_pc4[data.pop_gdf_nl_pc4["in_city"] == True]
         self.pop_dict_pc4_city = {pc4: pop for pc4, pop in zip(gdf.index, gdf["aantal_inwoners"])}
@@ -25,9 +35,28 @@ class UrbanModel(Model):
         for i in range(n_agents):
             self.agents.add(Traveler(i, self, locations[i], gdf["65x65 Nummer"][locations[i]]))
 
+        # Request agents to 
+        self.agents.do("generate_trip_times")
+        print(f"Events scheduled for agents: {self.simulator.event_list}")
+        # Schedule a model step
+        self.simulator.schedule_event_now(self.step)
+
     def step(self):
-        pass
+        # Print the current time
+        print(f"Model step (time: {self.simulator.time})")
+        # A step is considerd once the step_time. Default is 1/12 hour (5 minutes).
+
         # For each agent, initialize times they want to create a trip
         # Add those to the discrete event scheduler to
 
-model1 = UrbanModel()
+        # Schedule next even
+        self.simulator.schedule_event_relative(function=self.step, time_delta=self.step_time)
+
+
+simulator1 = DEVSimulator()
+model1 = UrbanModel(n_agents=5, simulator=simulator1)
+simulator1.model = model1
+print(f"Running the model from {model1.start_time} to {model1.end_time}")
+
+simulator1.run_until(model1.end_time)
+print(f"Model finished at {model1.simulator.time}")
