eb1b16db5e07d33f81d7fceda0c7e8a6a5c45ca3
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2024-10-08 22:10:25 +0200

    model: Update print statement and default variables

--- model/model.py
+++ model/model.py
@@ -21,7 +21,8 @@ class UrbanModel(Model):
     def __init__(self, step_time=1/12, start_time=5, end_time=11, choice_model="rational_vot", enable_av=True, av_cost_factor=0.25, av_vot_factor=0.5, ext_vehicle_load=0.8, uxsim_platoon_size=10, car_comfort=0.5, bike_comfort=1.33, av_density=1.0, induced_demand=1.0, policy_tarif=0, policy_tarif_time="peak", policy_speed_reduction=0, policy_area="autoluw", simulator=None):
         super().__init__()
         n_agents = int(real_population / uxsim_platoon_size)
-        print(f"### Initializing UrbanModel with {n_agents} agents, step time {step_time:.3f} hours, start time {start_time}, end time {end_time}, choice model {choice_model}, AV enabled {enable_av}, AV cost factor {av_cost_factor}, AV VOT factor {av_vot_factor}, external vehicle load {ext_vehicle_load}, UXsim platoon size {uxsim_platoon_size}, car comfort {car_comfort}, bike comfort {bike_comfort}, av density {av_density}, induced demand {induced_demand}.")
+        print(f"### Initializing UrbanModel with {n_agents} agents, step time {step_time:.3f} hours, start time {start_time}, end time {end_time}, choice model {choice_model}, AV enabled {enable_av}, AV cost factor {av_cost_factor}, AV VOT factor {av_vot_factor}, external vehicle load {ext_vehicle_load}, UXsim platoon size {uxsim_platoon_size}, car comfort {car_comfort}, bike comfort {bike_comfort},"
+              f"av density {av_density}, induced demand {induced_demand}, policy tarif {policy_tarif}, policy tarif time {policy_tarif_time}, policy speed reduction {policy_speed_reduction}, policy area {policy_area}.")
         # Set up simulator time
         self.n_agents = n_agents
         self.simulator = simulator
